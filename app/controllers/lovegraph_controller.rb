f1b7996b7a40fb4cd6c7596a8ddcaa8af15a4ea9
EC2 Default User <ec2-user@ip-172-31-46-159.us-east-2.compute.internal>
2018-01-06 08:48:12 +0000

    first commit

---
+++ app/controllers/lovegraph_controller.rb
@@ -0,0 +1,14 @@
+class LovegraphController < ApplicationController
+  def top
+  end
+  def about
+  end
+  def flow
+  end
+  def photos
+  end
+  def staffs
+  end
+  def login
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'lovegraph/top'
+  get 'lovegraph/about'
+  get 'lovegraph/flow'
+  get 'lovegraph/photos'
+  get 'lovegraph/staffs'
+  get 'lovegraph/login'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
